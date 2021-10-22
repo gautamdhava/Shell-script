2edd2c3178e7af2e815f39e6fdd33b0be8861a20
Gautam Dhavamani <gautamdhava@gmail.com>
2021-10-22 10:34:52 +0530

    Update check_mem_util.sh

--- check_mem_util.sh
+++ check_mem_util.sh
@@ -8,3 +8,4 @@ s1=`grep -w SwapFree /proc/meminfo | awk '{print $(NF-1)}'`
 s2=`grep -w SwapCached /proc/meminfo | awk '{print $(NF-1)}'`
 declare -i M_av="($m1+$m2)";Memory_available=`echo "$M_av" | awk '{ print $0/1024/1024 "G"}'`;declare -i Sw_av="($s1+$s2)";Swap_av=`echo "$Sw_av" | awk '{ print $0/1024/1024 "G"}'`
 echo " " ; free -h;echo " ";echo "Total_memory $totalmem" ; echo "Avail_memory $Memory_available";echo "Total_Swap $totalswap";echo Avail_Swap $Swap_av;echo "-----------------------------------";echo "Top Memory Consuming Processes";echo "------------------------------------" ; ps -eo pid,user,comm,%mem --sort=-%mem | head
+echo "test jenkins CI job"
